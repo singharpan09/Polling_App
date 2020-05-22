03790f7bfd560f5425273ccfdbb1ce8a3a0b1d67
singharpan09 <arpan@excellencetechnologies.info>
2020-05-22 12:30:11 +0530

    nav bar added

--- src/registrationForm.js
+++ src/registrationForm.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button, Navbar } from "react-bootstrap";
 
 const RegistrationForm = () => {
-  return <React.Fragment>This is Registration form Component</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#">Polling App</Navbar.Brand>
+
+        <Button variant="outline-success">Login</Button>
+      </Navbar>
+    </React.Fragment>
+  );
 };
 
 export default RegistrationForm;
