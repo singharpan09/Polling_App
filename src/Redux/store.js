e4a0b99b6438b148c100dc289e82981077f01d6d
singharpan09 <arpan@excellencetechnologies.info>
2020-05-22 16:04:30 +0530

    devtool implemented

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,8 @@
 import { createStore } from "redux";
 import reducer from "../Redux/reducers/reducer";
 
-const store = createStore(reducer);
+const reduxdevTool =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(reducer, reduxdevTool);
 
 export default store;
