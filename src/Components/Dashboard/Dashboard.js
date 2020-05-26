c32b51978a11bb1b29921aebe55a8ffeb95547e7
singharpan09 <arpan@excellencetechnologies.info>
2020-05-26 15:13:48 +0530

    poll list fetching successfully

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Dashboard = () => {
+  const [poll, setPoll] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://secure-refuge-14993.herokuapp.com/list_polls")
+      .then((res) => setPoll(res.data.data));
+  }, []);
+  console.log(poll);
   return (
     <React.Fragment>
       <center>
-        <h3>Welcome to Dashboard</h3>
+        <h3>Welcome to Poll</h3>
+        {poll.map((list) => (
+          <ol style={{ listStyleType: "number" }}>
+            <li>{list.title}</li>
+            {list.options.map((Option) => (
+              <ol>
+                <li>{Option.option}</li>
+              </ol>
+            ))}
+          </ol>
+        ))}
       </center>
     </React.Fragment>
   );
