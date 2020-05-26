950f8879a9d45305a34182414f60b69b87691e06
singharpan09 <arpan@excellencetechnologies.info>
2020-05-26 19:22:39 +0530

    createPoll added

--- src/Components/CreatePoll/CreatePoll.js
+++ src/Components/CreatePoll/CreatePoll.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import "./CreatePoll.css";
 
 const CreatePoll = () => {
+  const [title, settitle] = useState("");
+  const [options, setoptions] = useState([]);
   return (
     <React.Fragment>
-      <h3>Add new poll</h3>
+      <center>
+        <h3>Create New Poll</h3>
+      </center>
+      <div className="title">
+        <Form.Label>
+          <h2>Title</h2>
+        </Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter title here"
+          value={title}
+          onChange={(e) => settitle(e.target.value)}
+        />
+      </div>
     </React.Fragment>
   );
 };
