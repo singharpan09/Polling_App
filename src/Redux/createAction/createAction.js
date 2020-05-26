cbb54ce18e9ad65380b2c49ab99e4d3135402c59
singharpan09 <arpan@excellencetechnologies.info>
2020-05-26 15:38:17 +0530

    action creator for list poll

--- src/Redux/createAction/createAction.js
+++ src/Redux/createAction/createAction.js
@@ -8,3 +8,7 @@ export const RegistationError = createAction(actions.Registation_Error);
 export const LoginRequest = createAction(actions.Login_Request);
 export const LoginSuccess = createAction(actions.Login_Sucess);
 export const LoginError = createAction(actions.Login_Error);
+
+export const AddPollRequest = createAction(actions.Add_PollRequest);
+export const AddPollSuccess = createAction(actions.Add_PollSuccess);
+export const AddPollError = createAction(actions.Add_PollError);
