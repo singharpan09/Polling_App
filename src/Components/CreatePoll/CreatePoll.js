80003023231c1f00e226b32dce6703214e7b4e2d
singharpan09 <arpan@excellencetechnologies.info>
2020-05-27 12:10:59 +0530

    add button added

--- src/Components/CreatePoll/CreatePoll.js
+++ src/Components/CreatePoll/CreatePoll.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import "./CreatePoll.css";
 
 const CreatePoll = () => {
   const [title, settitle] = useState("");
   const [options, setoptions] = useState([]);
+  console.log(title);
   return (
     <React.Fragment>
-      <center>
-        <h3>Create New Poll</h3>
-      </center>
-      <div className="title">
+      <div className="poll">
+        <center>
+          <h3>Create New Poll</h3>
+        </center>
+      </div>
+
+      <div className="title1">
         <Form.Label>
           <h2>Title</h2>
         </Form.Label>
@@ -21,6 +25,11 @@ const CreatePoll = () => {
           onChange={(e) => settitle(e.target.value)}
         />
       </div>
+      {/* <div className="options">
+        <Form.Label>Option</Form.Label>
+        <Form.Control type="text" placeholder="Enter option here" />
+      </div> */}
+      {title ? <Button variant="primary">Add Option</Button> : null}
     </React.Fragment>
   );
 };
