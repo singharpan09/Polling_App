2b3464e0389daf447ec143835cdb0008d0e089e1
singharpan09 <arpan@excellencetechnologies.info>
2020-06-02 19:34:43 +0530

    reducer /few other changes

--- src/Redux/reducers/combinereducer.js
+++ src/Redux/reducers/combinereducer.js
@@ -5,12 +5,15 @@ import Loginreducer from "./Loginreducer";
 import PollListreducer from "./PollListreducer";
 import CreateNewPollreducer from "./createNewPollreducer";
 import UpdateTitlereducer from "./UpdatePoll/UpdatePollTitle";
+
+import DeletePollreducer from "./UpdatePoll/DeletePoll";
 const rootReducer = combineReducers({
   Registrationstatus: Registrationreducer,
   Loginstatus: Loginreducer,
   PollListstatus: PollListreducer,
   CreateNewPollstatus: CreateNewPollreducer,
   UpdateTitlestatus: UpdateTitlereducer,
+  DeletePollstatus: DeletePollreducer,
 });
 
 export default rootReducer;
